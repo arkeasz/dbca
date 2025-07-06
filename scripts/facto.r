abda0d2a220ac3e7d442b9f83e85d4a95e92801f
arki <antoniorafaelariasromero@gmail.com>
2025-07-06 00:02:20 -0500

    first commit

---
+++ scripts/facto.r
@@ -0,0 +1,7 @@
+install.packages("readxl");
+library(readxl)
+
+data <- read_xlsx("./scripts/COBRE.xlsx")
+View(data)
+
+# https://www.mdpi.com/2071-1050/11/22/6215?utm_source=chatgpt.com#table_body_display_sustainability-11-06215-t001
